0b8f50c5cb0b6cce9934a7c6289cf514b04e772f
Avila <avila111@avaya.com>
2022-05-16 19:00:21 -0500

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+import { Children } from "react";
+
+const Texto = styled.div `
+    background-color: #b7322c;
+    color: #fff;
+    padding: 15px;
+    font-size: 22px;
+    text-transform: uppercase;
+    font-weight: 700;
+    text-align: center;
+`
+
+
+const Error = ({children}) => {
+    return (  
+        <Texto>
+            {children} 
+        </Texto> 
+    );
+}
+ 
+export default Error;
---
+++ src/data/monedas.js
@@ -0,0 +1,27 @@
+const monedas = [
+    {   id: 'USD',
+        nombre: 'Dolar de Estados Unidos'
+    },
+    {   id: 'MXN',
+        nombre: 'Peso Mexicano'
+    },
+    {   id: 'EUR',
+        nombre: 'Euro'
+    },
+    {   id: 'GBP',
+        nombre: 'Libra Esterlina'
+    },
+    {   id: 'COP',
+    nombre: 'Peso Colombiano',
+    },
+    {   id: 'ARS',
+    nombre: 'Peso Argentino',
+    },
+    {   id: 'VES',
+    nombre: 'Bolivar',
+    },
+]
+
+export{
+    monedas
+}
