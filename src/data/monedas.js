6f7307af41ae7a3fb276b1e357ef45c11e3b24fb
Avila <avila111@avaya.com>
2022-05-16 19:06:39 -0500

    second commit

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -2,20 +2,22 @@ const monedas = [
     {   id: 'USD',
         nombre: 'Dolar de Estados Unidos'
     },
-    {   id: 'MXN',
-        nombre: 'Peso Mexicano'
-    },
+    
     {   id: 'EUR',
         nombre: 'Euro'
     },
     {   id: 'GBP',
         nombre: 'Libra Esterlina'
     },
+    {   id: 'ARS',
+    nombre: 'Peso Argentino',
+    },
     {   id: 'COP',
     nombre: 'Peso Colombiano',
     },
-    {   id: 'ARS',
-    nombre: 'Peso Argentino',
+    
+    {   id: 'MXN',
+        nombre: 'Peso Mexicano'
     },
     {   id: 'VES',
     nombre: 'Bolivar',
